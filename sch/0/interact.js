2e68a6a33454508e05d542983a8827567593f65d
MrARM <jordan@thesmokinggnu.us>
2016-08-26 14:03:43 -0500

    Update interact.js

--- sch/0/interact.js
+++ sch/0/interact.js
@@ -1,73 +1,7 @@
-$(function() {
-    var anim = false;
-    function typed(finish_typing) {
-        return function(term, message, delay, finish) {
-            anim = true;
-            var prompt = term.get_prompt();
-            var c = 0;
-            if (message.length > 0) {
-                term.set_prompt('');
-                var interval = setInterval(function() {
-                    term.insert(message[c++]);
-                    if (c == message.length) {
-                        clearInterval(interval);
-                        // execute in next interval
-                        setTimeout(function() {
-                            // swap command with prompt
-                            finish_typing(term, message, prompt);
-                            anim = false
-                            finish && finish();
-                        }, delay);
-                    }
-                }, delay);
-            }
-        };
+$('#term').terminal(function(command, term) {
+    if (command == 'getmein') {
+        term.echo("Please wait, Jordan Bush...");
+    } else {
+        term.echo('unknown command');
     }
-    var typed_prompt = typed(function(term, message, prompt) {
-        // swap command with prompt
-        term.set_command('');
-        term.set_prompt(message + ' ');
-    });
-    var typed_message = typed(function(term, message, prompt) {
-        term.set_command('');
-        term.echo(message)
-        term.set_prompt(prompt);
-    });
-
-    $('body').terminal(function(cmd, term) {
-        var finish = false;
-        var msg = "Wait I'm executing ajax call";
-        term.set_prompt('> ');
-        typed_message(term, msg, 200, function() {
-            finish = true;
-        });
-        var args = {command: cmd};
-        $.get('commands.php', args, function(result) {
-            (function wait() {
-                if (finish) {
-                    term.echo(result);
-                } else {
-                    setTimeout(wait, 500);
-                }
-            })();
-        });
-    }, {
-        name: 'xxx',
-        greetings: null,
-        width: 500,
-        height: 300,
-        onInit: function(term) {
-            // first question
-            var msg = "Wellcome to my terminal";
-            typed_message(term, msg, 200, function() {
-                typed_prompt(term, "what's your name:", 100);
-            });
-        },
-        keydown: function(e) {
-            //disable keyboard when animating
-            if (anim) {
-                return false;
-            }
-        }
-    });
-});
+}, { prompt: 'vault>', name: 'sch0' });
