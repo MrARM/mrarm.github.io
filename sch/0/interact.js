761785fa393d632841dc44f4b078ebe0698b54cd
MrARM <jordan@thesmokinggnu.us>
2016-08-27 13:31:51 -0500

    Update interact.js

--- sch/0/interact.js
+++ sch/0/interact.js
@@ -1,7 +1,9 @@
-$('#term').terminal(function(command, term) {
-    if (command == 'getmein') {
-        term.echo("Please wait, Jordan Bush...");
-    } else {
-        term.echo('unknown command');
-    }
-}, { prompt: 'vault>', name: 'sch0' });
+jQuery(document).ready(function($) {
+            $('body').terminal(function(command, term) {
+                if (command == 'getmein') {
+                    term.echo("Please wait, Jordan Bush...");
+                } else {
+                    term.echo('unknown command');
+                }
+            }, { prompt: 'vault>', name: 'sch0' });
+        });
