ead8f64645ff8acadd489f7ce971d5ed60b563fc
MrARM <jordan@thesmokinggnu.us>
2016-08-27 14:57:57 -0500

    Update mrarm_util.js

--- sch/0/mrarm_util.js
+++ sch/0/mrarm_util.js
@@ -3,3 +3,13 @@ function wait(mili) {
    while (Time + mili >= new Date().getTime()) {
    }
 }
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
