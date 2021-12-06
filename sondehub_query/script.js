d1d364f6cf1cfed70ae8fb232a16263d43cdfbcc
MrARM <jordan@thesmokinggnu.us>
2021-12-06 17:46:14 -0600

    add heatmap

--- sondehub_query/script.js
+++ sondehub_query/script.js
@@ -22,7 +22,50 @@ const options = {
   minute: "2-digit"
 };
 
-const query = () => {
+const heatMapper = data => {
+    const sondeMap = [];
+    const cfg = {
+        // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+        // if scaleRadius is false it will be the constant radius used in pixels
+        radius: .15,
+        maxOpacity: .7,
+        // scales the radius based on map zoom
+        scaleRadius: true,
+        // if set to false the heatmap uses the global maximum for colorization
+        // if activated: uses the data maximum within the current map boundaries
+        //   (there will always be a red spot with useLocalExtremas true)
+        useLocalExtrema: false,
+        // which field name in your data represents the latitude - default "lat"
+        latField: 'lat',
+        // which field name in your data represents the longitude - default "lng"
+        lngField: 'lng',
+        // which field name in your data represents the data value - default "value"
+        valueField: 'count'
+    };
+
+    const heatmapLayer = new HeatmapOverlay(cfg);
+
+
+    // Create dataset for heatmap plugin
+    for(const [ser, dat] of Object.entries(data)){
+        const sondeData = {
+            lat: dat.lat,
+            lng: dat.lon,
+            count: 1
+        };
+        sondeMap.push(sondeData);
+    }
+
+    // Display
+    console.log(sondeMap);
+    heatmapLayer.setData({
+        max: 2,
+        data: sondeMap
+    });
+    map.addLayer(heatmapLayer);
+};
+
+const query = (heatmap) => {
   // Reset State
   map.eachLayer((layer) => {
     // Don't remove the tiles
@@ -45,6 +88,9 @@ const query = () => {
       map.flyTo([lat, lon], 9);
 
       console.log(`Sonde count: ${Object.entries(data).length}`);
+      if(heatmap){
+          return heatMapper(data); // Easier to reuse a query
+      }
       for (const [ser, dat] of Object.entries(data)) {
         // ~~SH seems to return anything, I only want things nearish the ground~~ Changed to show predictions
         //if (dat.alt > 5000) {
